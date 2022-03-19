cceed4abf9d3c5b0cd1b76a1876feabb75724081
Chanelle Huang <chanellehuang@gmail.com>
2022-03-19 05:27:05 -1000

    added back-end database and server

--- server.js
+++ server.js
@@ -1,3 +1,49 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers_choice_full_stack');
+const faker = require('faker');
+
+const Company = sequelize.define('company', {
+  name: {
+    type: Sequelize.DataTypes.STRING(),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  catchPhrase: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.DataTypes.STRING(1000),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  phone: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+const syncAndSeed = async () => {
+  await sequelize.sync({ force: true });
+  await Promise.all([
+    Company.create({name: faker.company.companyName(), catchPhrase: faker.company.catchPhrase(), description: faker.lorem.paragraph(), phone: faker.phone.phoneNumber()}),
+    Company.create({name: faker.company.companyName(), catchPhrase: faker.company.catchPhrase(), description: faker.lorem.paragraph(), phone: faker.phone.phoneNumber()}),
+    Company.create({name: faker.company.companyName(), catchPhrase: faker.company.catchPhrase(), description: faker.lorem.paragraph(), phone: faker.phone.phoneNumber()}),
+    Company.create({name: faker.company.companyName(), catchPhrase: faker.company.catchPhrase(), description: faker.lorem.paragraph(), phone: faker.phone.phoneNumber()}),
+    Company.create({name: faker.company.companyName(), catchPhrase: faker.company.catchPhrase(), description: faker.lorem.paragraph(), phone: faker.phone.phoneNumber()})
+  ]);
+}
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,6 +51,24 @@ const path = require('path');
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-const port = process.env.PORT || 3000;
+app.get('/api/companies', async (req, res, next) => {
+  try {
+    res.send(await Company.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+const init = async () => {
+  try {
+    await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  }
+  catch(ex) {
+    console.log(ex);
+  }
+};
+
+init();
